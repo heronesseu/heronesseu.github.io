c4b3f8875f7f4eb0793539792df5acfa5bb68e43
heronesseu <144932944+heronesseu@users.noreply.github.com>
2025-03-15 04:46:08 +0800

    Initial upload of files
    test upload

---
+++ javascript/fun-fact.js
@@ -0,0 +1,7 @@
+function reveal() {
+    var imageDisp = document.getElementById("tea-cup");
+    var hiddenDiv = document.getElementById("fun-fact");
+
+    imageDisp.src = "images/tea_2.png";
+    hiddenDiv.removeAttribute("hidden");
+}
